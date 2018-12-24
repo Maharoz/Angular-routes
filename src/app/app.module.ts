d2e0d3e108b48e08afc9bbba0cd39803fcfbffc7
Maharoz <mahruj66@gmail.com>
2018-12-24 23:01:08 +0600

    first-commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { ServersService } from './servers/servers.service';
+import { ServerComponent } from './servers/server/server.component';
+import { UserComponent } from './users/user/user.component';
+import { EditServerComponent } from './servers/edit-server/edit-server.component';
+import { HomeComponent } from './home/home.component';
+import { UsersComponent } from './users/users.component';
+import { ServersComponent } from './servers/servers.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    UsersComponent,
+    ServersComponent,
+    UserComponent,
+    EditServerComponent,
+    ServerComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [ServersService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
