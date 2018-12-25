53f2b7e137831a602fb972ee41077ff5876c057f
Maharoz <mahruj66@gmail.com>
2018-12-25 11:48:01 +0600

    fetching routing parameter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,7 +18,7 @@ import { AppComponent } from './app.component';
 const appRoutes : Routes = [ 
   { path : '' , component : HomeComponent} ,
   { path : 'users' , component : UserComponent} ,
-  { path : 'users/:id' , component : UserComponent} ,
+  { path : 'users/:id/:name' , component : UserComponent} ,
   { path : 'servers' , component : ServersComponent} 
 ];
 
